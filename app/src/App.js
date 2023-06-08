0d12fb548057c52d156287c9505b17010b42e0b2
spenclark <spenclark@hotmail.com>
2023-06-07 22:24:31 -0700

    initail setup and components

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Hero from './Components/Hero';
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar />
+      <Hero />
     </div>
   );
 }
