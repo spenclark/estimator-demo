336ec95a62e1a7aa6182d202c2a09d9ebb1197e7
spenclark <spenclark@hotmail.com>
2023-06-07 22:28:32 -0700

    typo + index.css

--- app/src/Components/Hero.js
+++ app/src/Components/Hero.js
@@ -4,7 +4,7 @@ function Hero() {
   return (
     <div>
         <h1>Never have uncertainty on a bid again</h1>
-        <h4>EstimatorHero is an intelligent side-kick that uses public filing data to let you know how competivie your bid is with local competitors</h4>
+        <h4>EstimatorHero is an intelligent side-kick that uses public filing data to help you competitive your bid is with local business</h4>
     </div>
   )
 }
