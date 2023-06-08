a0b779781d3bb95e6065e97db795e842e4223ae5
spenclark <spenclark@hotmail.com>
2023-06-07 22:24:35 -0700

    Initial Components

---
+++ app/src/Components/Hero.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Hero() {
+  return (
+    <div>
+        <h1>Never have uncertainty on a bid again</h1>
+        <h4>EstimatorHero is an intelligent side-kick that uses public filing data to let you know how competivie your bid is with local competitors</h4>
+    </div>
+  )
+}
+
+export default Hero
---
+++ app/src/Components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+        <div>
+            <h1>EstimatorHero</h1>
+        </div>
+        <div>
+            <button>Try now</button>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
